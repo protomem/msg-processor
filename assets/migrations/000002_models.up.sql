67c933de0baa1df230aeefab9b46365c8796b6b0
protomem <protomem0709@gmail.com>
2024-07-23 12:06:26 +0300

    Migrations

---
+++ assets/migrations/000002_models.up.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS messages (
+    id BIGSERIAL PRIMARY KEY,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    message TEXT NOT NULL CHECK (length(message) > 0),
+    status  TEXT NOT NULL CHECK (status = 'created' OR status = 'processing' OR status = 'completed')
+);
+
+COMMIT;
